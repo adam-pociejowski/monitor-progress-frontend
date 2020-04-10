48daa970dca60a7fb8462da52436676a0a6b9224
Adam Pociejowski <valverde12345@gmail.com>
2020-04-10 19:02:31 +0200

    date format fix

--- src/app/core/pipe/short.date.pipe.ts
+++ src/app/core/pipe/short.date.pipe.ts
@@ -7,12 +7,13 @@ export class ShortDatePipe implements PipeTransform {
 
   transform(isoDate: string): any {
     let date = new Date(Date.parse(isoDate));
-    let result = date.getDay() < 10 ?
-      '0' + date.getDay() :
-      date.getDay()+'';
-    result = date.getMonth() < 10 ?
-      result + '/0' + date.getMonth() :
-      result + '/' + date.getMonth();
+    let result = date.getDate() < 10 ?
+      '0' + date.getDate() :
+      date.getDate()+'';
+    let month = date.getMonth() + 1;
+    result = month < 10 ?
+      result + '/0' +month :
+      result + '/' + month;
     return result + '/' + (date.getFullYear()-2000);
   }
 }
