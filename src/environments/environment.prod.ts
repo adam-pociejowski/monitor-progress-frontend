87a1133d6dab5e7e52d6be6ee6f66c04be931445
Valverde <valverde12345@gmail.com>
2020-05-06 22:44:39 +0200

    modified ouath login

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backend_url: 'https://monitoring-progress-backend.valverde.duckdns.org'
+  backend_url: 'https://monitoring-progress-backend.apociejowski.pl'
 };
